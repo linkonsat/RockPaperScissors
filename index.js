b30d7748bf246e04a4923ab3423112f6f2f05497
mmaisel <macdanielrmaisel@gmail.com>
2021-06-17 07:26:14 -0700

    final changes for game functions

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-// I get it, you're curious, 
-// but it's OK if you don't understand what's going on in here, you'll learn it in time.
+<script>
+	
+	
+	
+	
+	
+function computerPlay() {
+		let computerdraws = ["rock", "paper", "Scissors"];
+		let computerdrawrandom = computerdraws[Math.floor(Math.random() * computerdraws.length)];
 
-const abTroubleshoot = require("./troubleshooting");
+		console.log(computerdrawsrandom * 5);
+		}
+	
+	function playRound(playerSelection, computerSelection){	
+	const playerSelection = prompt("Please enter your hand for this round");
+	let i = 0
+	
 
-const result = abTroubleshoot();
 
-if(result === 2) {
-	console.log("Congrats! You got the correct answer");
-} else if(typeof result === 'number') {
-	console.log(`You returned the number ${result}, the result should be the number 2`);
-} else {
-	console.log(`You returned string "${result}", the result should be the number 2`);
-}
+	
+	if (computerdrawrandom == "rock" && playerSelection =="rock") {
+		alert("its a tie")
+	} else if ( computerdrawrandom == "rock" &&  playerSelection == "paper") {
+		alert("its a tie")
+		i+= 1
+
+	}   else if (computerdrawrandom == "rock" &&  playerSelection == "scissors") {
+	alert("you lose")
+	} else if (computerdrawrandom == "paper" && playerSelection == "scissors") {
+		alert("you win")
+		i += 1
+	}	else if (computerdrawrandom == "paper" && playerSelection == "rock"){
+	alert("you lose")
+	} else if (computerdrawrandom ==  "paper" && playerSelection == "paper")
+	alert("this is a tie")
+	 else if (computerdrawrandom == "scissors" && playerSelection == "scissors") {
+		alert ("this is a tie")
+	} else if  (computerdrawrandom == "scissors" && playerSelection == "rock") {
+		alert(" this is a win. rock beats scissors")
+		i += 1
+	} else if (computerdrawrandom == "scissors" || playerSelection == "paper") {
+		alert("This is a loss scissors beats paper")
+	}
+
+		
+	
+
+	
+	
+
+	
+		
+	
+
+</script>
