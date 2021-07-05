f780838d5290e3515a4a47537171f90661a31718
mmaisel <macdanielrmaisel@gmail.com>
2021-07-04 21:26:37 -0700

    added game description title

--- java.js
+++ java.js
@@ -1,32 +1,42 @@
-<script>
+
 	
-	let playerSelection = playermove();
+	
+	
+let playerSelection = playermove();
 	let computerSelection= computerplay();
 	let roundresult = playRound(playerSelection, computerSelection);
+
+
 	function computerplay() {
 		let computerdraws = ["rock", "paper", "scissors"];
 		return computerdraws[Math.floor(Math.random() * computerdraws.length)];
-	}
+		}
 	function playermove() {
-		return window.prompt("Please enter:rock,paper, or scissors").toLowerCase();
+		
+			return window.prompt("Please enter:rock,paper, or scissors").toLowerCase();
 	}
 	function playRound(playerSelection,computerSelection) {
 	
-	
 	if ( computerSelection == "rock" &&  playerSelection == "paper") {
 		return 1;
+		
 	}   else if (computerSelection == "rock" &&  playerSelection == "scissors") {
-		return 2;
+		return 2;		
 	} else if (computerSelection == "paper" && playerSelection == "scissors") {
 		return 1;
+	
 	}	else if (computerSelection == "paper" && playerSelection == "rock"){
-	return  2;
+		return 2;
+		
 	}  else if  (computerSelection == "scissors" && playerSelection == "rock") {
 		return 1;
+	
 	} else if (computerSelection == "scissors" && playerSelection == "paper") {
 		return 2;
 		
 	} 
+	
+
 	}
 
 	function announcer(playRound,playerSelection,computerSelection) {
@@ -34,35 +44,42 @@
 		return "You win. Player choice: " + playerSelection + " vs Computer choice: " + computerSelection;
 	} else if (playRound == 2) {
 		return "You lose. player choice: " + playerSelection + " vs Computer choice: " + computerSelection;
-	} else if (playRound == undefined) {
+	} else if (playRound == undefine) {
 		return "No win or loss" + playerSelection + computerSelection
 	}
 	}
 
 
 	function game(roundresult){
-		let playerscore = 0;
-		for (let i = 0; i <= 4; i++){
-		++playerscore;
-		}
+		let playerscore = (function() {
+			let playerscore = 0;
+			return function() {
+				++playerscore;
+				return playerscore;
+			}
+		})();
 
-		if (playerscore == 5) {
-			return "You win the game!"
-		} 
-		//else if (computerscore == 5) {
-		//	return "You lose the game!"
-		//}
+		let computerscore = (function() {
+			let computerscore = 0;
+			return function() {
+				++computerscore;
+				return computerscore;
+			}
+		})();
+
+		if (roundresult == 1) {
+			playerscore();
+			
+		} else if (roundresult ==2 ) {
+			computerscore ();
+			
 		}
 		
-
+		if (computerscore == 5) {
+			return "you lose!";
+		} else if (playerscore == 5) {
+			return "return you win!"
+		}
+		
+		}
 	
-
-
-
-
-	
-	
-	
-	
-
-</script>
